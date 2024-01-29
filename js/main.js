b1617c8079befe694383bcb79c3184fe6d5f4420
woohyeoklee <whyeok94@gmail.com>
2024-01-29 19:00:33 +0900

    complete

--- js/main.js
+++ js/main.js
@@ -73,38 +73,76 @@ new Swiper('.promotion .swiper-container', {
   }
 });
 
-const promotionEl = document.querySelector('.promotion');
-const promotionToggleBtn = document.querySelector('.toggle-promotion');
-let isHidePromotion = false; // 숨김 처리 여부 기본값은 false
+/**
+ * Promotion 슬라이드 토글 기능
+ */
+// 슬라이드 영역 요소 검색!
+const promotionEl = document.querySelector('.promotion')
+// 슬라이드 영역를 토글하는 버튼 검색!
+const promotionToggleBtn = document.querySelector('.toggle-promotion')
+// 슬라이드 영역 숨김 여부 기본값!
+let isHidePromotion = false
+// 토글 버튼을 클릭하면,
 promotionToggleBtn.addEventListener('click', function () {
-  isHidePromotion = !isHidePromotion // !는 반대값으로 바꿔줌
+  // 슬라이드 영역 숨김 여부를 반댓값으로 할당!
+  isHidePromotion = !isHidePromotion
+  // 요소를 숨겨야 하면,
   if (isHidePromotion) {
-    // 숨김 처리
-    promotionEl.classList.add('hide');
+    promotionEl.classList.add('hide')
+  // 요소가 보여야 하면,
   } else {
-    // 보임 처리
-    promotionEl.classList.remove('hide');
+    promotionEl.classList.remove('hide')
   }
-});
+})
 
+
+/**
+ * 부유하는 요소 관리
+ */
 // 범위 랜덤 함수(소수점 2자리까지)
 function random(min, max) {
-  // `.toFixed()`를 통해 반환된 문자 데이터를,
-  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  // `.toFixed()`를 통해 반환된 '문자 데이터'를,
+  // `parseFloat()`을 통해 소수점을 가지는 '숫자 데이터'로 변환
   return parseFloat((Math.random() * (max - min) + min).toFixed(2))
 }
+// 부유하는(떠 다니는) 요소를 만드는 함수
 function floatingObject(selector, delay, size) {
   gsap.to(
-    selector,
-    random(1.5, 2.5), 
+    selector, // 선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
     {
-      y: 20,
-      repeat: -1, // 무한반복
-      yoyo: true, // 한번 재생된 애니메이션을 다시 뒤로 재생
-      ease: Power1.easeInOut,
-      delay: random(0, delay)
-    });
+      delay: random(0, delay), // 얼마나 늦게 애니메이션을 시작할 것인지 지연 시간을 설정.
+      y: size, // `transform: translateY(수치);`와 같음. 수직으로 얼마나 움직일지 설정.
+      repeat: -1, // 몇 번 반복하는지를 설정, `-1`은 무한 반복.
+      yoyo: true, // 한번 재생된 애니메이션을 다시 뒤로 재생.
+      ease: Power1.easeInOut // Easing 함수 적용.
+    }
+  )
 }
-floatingObject('.floating1', 1, 15);
-floatingObject('.floating2', .5, 15);
-floatingObject('.floating3', 1.5, 20);
+floatingObject('.floating1', 1, 15)
+floatingObject('.floating2', .5, 15)
+floatingObject('.floating3', 1.5, 20)
+
+
+/**
+ * 요소가 화면에 보여짐 여부에 따른 요소 관리
+ */
+// 관리할 요소들 검색!
+const spyEls = document.querySelectorAll('section.scroll-spy')
+// 요소들 반복 처리!
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({ // 감시할 장면(Scene)을 추가
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
+    })
+    .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
+})
+
+
+/**
+ * 올해가 몇 년도인지 계산
+ */
+const thisYear = document.querySelector('.this-year')
+thisYear.textContent = new Date().getFullYear()
